8aad05a11dd64a71b931d11b816c8dabf2468e99
Duarte Lucas <duarteprista@gmail.com>
2017-02-16 18:48:59 +0000

    need to create actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216143430) do
+ActiveRecord::Schema.define(version: 20170216151156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "cocktails", force: :cascade do |t|
     t.string   "name"
@@ -28,10 +21,22 @@ ActiveRecord::Schema.define(version: 20170216143430) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "ingredients", force: :cascade do |t|
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table "doses", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "ingredient_id"
+    t.integer  "cocktail_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id", using: :btree
+    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id", using: :btree
   end
 
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
 end
